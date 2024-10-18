c920f87cfcb508967ea4e617a6789df05c8f7e8c
GEdO23 <rm99632@fiap.com.br>
2024-10-18 17:14:02 -0300

    My First Composable

--- app/src/main/java/br/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/MainActivity.kt
@@ -3,38 +3,21 @@ package br.com.alura.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.MutableTransitionState
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             AluveryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    MyFirstComposable()
                 }
             }
         }
@@ -42,17 +25,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MyFirstComposable(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Meu primeiro texto",
+        modifier = modifier
+    )
+    Text(
+        text = "Meu segundo texto maior",
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    AluveryTheme() {
-        Greeting("Android", Modifier.fillMaxSize())
+fun MyFirstComposablePreview() {
+    AluveryTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MyFirstComposable()
+        }
     }
 }
