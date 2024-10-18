9779b73619d9881422db93336b358bdc5a6a5d30
GEdO23 <rm99632@fiap.com.br>
2024-10-18 18:30:00 -0300

    Creating Product Item structure
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/MainActivity.kt
@@ -3,8 +3,20 @@ package br.com.alura.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
-import br.com.alura.aluvery.ui.components.CustomLayoutPreview
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,10 +25,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-//                    MyFirstComposable()
-                    CustomLayoutPreview()
+                    ProductItem()
                 }
             }
         }
     }
+}
+
+@Composable
+fun ProductItem() {
+    Column {
+        Box(modifier = Modifier
+            .height(50.dp)
+            .background(Color.Blue)
+            .width(50.dp))
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Product's Image"
+        )
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
+    }
+}
+
+@Preview(group = "ProductComponents", showBackground = true)
+@Composable
+private fun ProductItemPreview() {
+    ProductItem()
 }
