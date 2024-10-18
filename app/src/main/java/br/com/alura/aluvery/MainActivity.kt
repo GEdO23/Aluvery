a7d262ad1bcf03aac977776b4340a9bafaf35519
GEdO23 <rm99632@fiap.com.br>
2024-10-18 17:15:40 -0300

    Layout Composables

--- app/src/main/java/br/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/MainActivity.kt
@@ -3,6 +3,9 @@ package br.com.alura.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +22,56 @@ class MainActivity : ComponentActivity() {
                 Surface {
                     MyFirstComposable()
                 }
+            }
+        }
+    }
+}
+
+@Preview(group = "LayoutComposables", showBackground = true)
+@Composable
+private fun ColumnPreview() {
+    Column { 
+        Text("Texto 1")
+        Text("Texto 2")
+    }
+}
+
+@Preview(group = "LayoutComposables", showBackground = true)
+@Composable
+private fun RowPreview() {
+    Row { 
+        Text("Texto 1")
+        Text("Texto 2")
+    }
+}
+
+@Preview(group = "LayoutComposables", showBackground = true)
+@Composable
+private fun BoxPreview() {
+    Box {
+        Text("Texto 1")
+        Text("Texto 2")
+    }
+}
+
+@Preview(group = "LayoutComposables", showBackground = true)
+@Composable
+private fun CustomLayoutPreview() {
+    Column { 
+        Text("Texto 1")
+        Text("Texto 2")
+        Row { 
+            Text("Texto 3")
+            Text("Texto 4")
+        }
+        Box {
+            Row {
+                Text("Texto 5")
+                Text("Texto 6")
+            }
+            Column {
+                Text("Texto 7")
+                Text("Texto 8")
             }
         }
     }
