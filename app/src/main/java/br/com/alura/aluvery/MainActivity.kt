d782aa20833b2b80e5419cc66042d4ea49de406a
GEdO23 <rm99632@fiap.com.br>
2024-10-18 19:08:16 -0300

    Applying gradient to Product Item header
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/MainActivity.kt
@@ -7,17 +7,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.alura.aluvery.ui.theme.AluveryTheme
+import br.com.alura.aluvery.ui.theme.Purple500
+import br.com.alura.aluvery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,11 +37,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ProductItem() {
-    Column {
-        Box(modifier = Modifier
-            .height(50.dp)
-            .background(Color.Blue)
-            .width(50.dp))
+    Column(
+        Modifier
+            .height(250.dp)
+            .width(200.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .background(brush = Brush.horizontalGradient(listOf(Teal200, Purple500)))
+                .fillMaxWidth()
+        )
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
             contentDescription = "Product's Image"
