12fde3be9fe4a4096987af1e13cb8e9f31607046
GEdO23 <rm99632@fiap.com.br>
2024-10-18 18:02:04 -0300

    Modifier Composable
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/MainActivity.kt
@@ -3,15 +3,21 @@ package br.com.alura.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +26,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    MyFirstComposable()
+//                    MyFirstComposable()
+                    CustomLayoutPreview()
                 }
             }
         }
@@ -30,7 +37,7 @@ class MainActivity : ComponentActivity() {
 @Preview(group = "LayoutComposables", showBackground = true)
 @Composable
 private fun ColumnPreview() {
-    Column { 
+    Column {
         Text("Texto 1")
         Text("Texto 2")
     }
@@ -39,7 +46,7 @@ private fun ColumnPreview() {
 @Preview(group = "LayoutComposables", showBackground = true)
 @Composable
 private fun RowPreview() {
-    Row { 
+    Row {
         Text("Texto 1")
         Text("Texto 2")
     }
@@ -54,22 +61,56 @@ private fun BoxPreview() {
     }
 }
 
+@Preview(group = "ModifierComposables", showBackground = true, showSystemUi = true)
 @Preview(group = "LayoutComposables", showBackground = true)
 @Composable
 private fun CustomLayoutPreview() {
-    Column { 
+    Column(
+        Modifier
+            .padding(8.dp)
+            .background(color = Color.Blue)
+            .padding(all = 8.dp)
+//        .height(400.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
         Text("Texto 1")
         Text("Texto 2")
-        Row { 
+        Row(
+            modifier = Modifier
+                .padding(
+                    horizontal = 8.dp,
+                    vertical = 16.dp
+                )
+                .background(Color.Green)
+                .fillMaxWidth(0.9f)
+        ) {
             Text("Texto 3")
             Text("Texto 4")
         }
-        Box {
-            Row {
+        Box(
+            Modifier
+                .padding(8.dp)
+                .background(color = Color.Red)
+                .padding(all = 8.dp)
+//            .width(100.dp)
+        ) {
+            Row(
+                Modifier
+                    .padding(8.dp)
+                    .background(color = Color.Cyan)
+                    .padding(all = 8.dp)
+                    .fillMaxWidth()
+            ) {
                 Text("Texto 5")
                 Text("Texto 6")
             }
-            Column {
+            Column(
+                Modifier
+                    .padding(8.dp)
+                    .background(color = Color.Yellow)
+                    .padding(all = 8.dp)
+            ) {
                 Text("Texto 7")
                 Text("Texto 8")
             }
