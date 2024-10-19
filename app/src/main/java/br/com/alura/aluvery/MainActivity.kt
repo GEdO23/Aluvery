07002d751d439ef4b13a4c66b2379535cfde13f3
GEdO23 <rm99632@fiap.com.br>
2024-10-18 21:58:20 -0300

    Adjustings Product Item texts
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -22,8 +23,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 import br.com.alura.aluvery.ui.theme.Purple500
 import br.com.alura.aluvery.ui.theme.Teal200
@@ -65,8 +70,21 @@ fun ProductItem() {
             )
         }
         Spacer(modifier = Modifier.height(50.dp))
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = LoremIpsum(50).values.first(),
+                fontSize = 18.sp,
+                fontWeight = FontWeight(700),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "R$ 14,99",
+                modifier = Modifier.padding(top = 8.dp),
+                fontSize = 14.sp,
+                fontWeight = FontWeight(400)
+            )
+        }
     }
 }
 
