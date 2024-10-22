cba8a2b87adb7851b53b023aec44ee1a4daf7d90
GEdO23 <rm99632@fiap.com.br>
2024-10-22 15:52:29 -0300

    Applying horizontal scroll on ProductsSection

--- app/src/main/java/br/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -17,6 +19,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
@@ -44,7 +47,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    ProductsSection()
+                    Column {
+                        ProductsSection()
+                        ProductsSection()
+                        ProductsSection()
+                    }
                 }
             }
         }
@@ -60,15 +67,18 @@ fun ProductsSection() {
             fontSize = 20.sp,
             fontWeight = FontWeight(400)
         )
-        Row(Modifier.fillMaxWidth().padding(
-            start = 16.dp,
-            top = 8.dp,
-            end = 16.dp,
-            bottom = 16.dp
-        ), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp, bottom = 16.dp)
+                .horizontalScroll(rememberScrollState()),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Spacer(Modifier)
             ProductItem()
             ProductItem()
             ProductItem()
+            Spacer(Modifier)
         }
     }
 }
@@ -118,16 +128,26 @@ fun ProductItem(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(group = "ProductsComponents", showBackground = true, widthDp = 1000)
 @PreviewLightDark
 @Composable
 private fun ProductsSectionPreview() {
-    ProductsSection()
+    AluveryTheme {
+        Surface(Modifier.fillMaxSize()) {
+            Column {
+                ProductsSection()
+                ProductsSection()
+                ProductsSection()
+            }
+        }
+    }
 }
 
-@Preview(group = "ProductsComponents", showBackground = true)
-@PreviewLightDark
+@Preview
 @Composable
 private fun ProductItemPreview() {
-    ProductItem()
+    AluveryTheme {
+        Surface {
+            ProductItem(Modifier.padding(16.dp))
+        }
+    }
 }
