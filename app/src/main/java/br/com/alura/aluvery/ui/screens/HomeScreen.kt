c7b015efcbb3aa492f7e354be326da3ad490f3a2
GEdO23 <rm99632@fiap.com.br>
2024-11-05 19:57:20 -0300

    Flexibilizando a seção de produtos

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -11,7 +11,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.com.alura.aluvery.R
+import br.com.alura.aluvery.model.Product
+import br.com.alura.aluvery.sampledata.sampleProducts
 import br.com.alura.aluvery.ui.components.ProductsSection
+import java.math.BigDecimal
 
 @Composable
 fun HomeScreen() {
@@ -22,9 +26,24 @@ fun HomeScreen() {
         verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Top)
     ) {
         Spacer(Modifier)
-        ProductsSection()
-        ProductsSection()
-        ProductsSection()
+        ProductsSection(
+            title = "Promoções",
+            products = sampleProducts
+        )
+        ProductsSection(
+            title = "Doces",
+            products = listOf(
+                Product(
+                    name = "Chocolate",
+                    price = BigDecimal("5.99"),
+                    image = R.drawable.placeholder
+                )
+            )
+        )
+        ProductsSection(
+            title = "Bebidas",
+            products = sampleProducts
+        )
         Spacer(Modifier)
     }
 }
